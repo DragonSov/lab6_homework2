a6af0901f854f0329574f513d5c1048ab16b8f1a
stpov <dzen@DESKTOP-L7BK9S0>
2025-10-13 15:04:27 +0300

    Добавьте файлы проекта.

---
+++ main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+    setlocale(LC_CTYPE, "RUS");
+
+    double x, y;
+    int belonging;
+
+    printf("Введите координаты точки x y: ");
+    scanf("%lf %lf", &x, &y);
+
+    belonging = (x >= 2.0) &&
+        (4.0 * x + 3.0 * y <= 23.0) &&
+        (5.0 * x - 3.0 * y <= 22.0);
+
+    if (belonging)
+    {
+		printf("Точка (%.1f; %.1f) принадлежит заштрихованной области.\n", x, y);
+    }
+    else
+    {
+        printf("Точка (%.1f; %.1f) не принадлежит заштрихованной области.\n", x, y);
+    }
+    return 0;
+}
